63bd36f1db8cf707744217b6f8d87ebc78f72a8a
Nicholas Narsing <soren121@sorenstudios.com>
2017-03-22 17:13:23 -0400

    Fix map and styling

--- webresources/js/google-map.js
+++ webresources/js/google-map.js
@@ -1,13 +1,24 @@
 function initMap() {
-    var uluru = {lat: -25.363, lng: 131.044};
-    
-    var map = new google.maps.Map(document.getElementById('google-map'), {
-        zoom: 4,
-        center: uluru
+    var mapEl = document.querySelector('#google-map');
+    // Show all of US in default view
+    var map = new google.maps.Map(mapEl, {
+        center: {lat: 39.05, lng: -94.34},
+        zoom: 5
     });
 
-    var marker = new google.maps.Marker({
-        position: uluru,
-        map: map
-    });
+    // Ask user for their location automatically
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            // Set the map center to the user's location
+            map.setCenter({
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            });
+
+            // Zoom in
+            map.setZoom(12);
+        }, function() {
+            alert('Geolocation failed :(');
+        });
+    }
 }
