6747803abc8438b43706a2e4c244ac0f8ebf13ed
Nicholas Narsing <soren121@sorenstudios.com>
2017-03-23 00:29:47 -0400

    Optimize hex operations in PasswordHash

--- src/main/java/io/github/rallyme/PasswordHash.java
+++ src/main/java/io/github/rallyme/PasswordHash.java
@@ -6,31 +6,20 @@ import javax.crypto.SecretKeyFactory;
 import java.math.BigInteger;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
+import javax.xml.bind.DatatypeConverter;
 
 public class PasswordHash {
-    public static final int SALT_BYTES = 24;
-    public static final int HASH_BYTES = 24;
-    public static final int PBKDF2_ITERATIONS = 5000;
+
+    public static final int SALT_LEN = 24; // bytes
+    public static final int HASH_LEN = 24; // bytes
+    public static final int ITERATIONS = 5000;
 
     private static byte[] fromHex(String hex) {
-        byte[] binary = new byte[hex.length() / 2];
-        for(int i = 0; i < binary.length; i++) {
-            binary[i] = (byte)Integer.parseInt(hex.substring(2 * i, 2 * i + 2), 16);
-        }
-
-        return binary;
+        return DatatypeConverter.parseHexBinary(hex);
     }
 
-    private static String toHex(byte[] array) {
-        BigInteger bi = new BigInteger(1, array);
-        String hex = bi.toString(16);
-        int paddingLength = (array.length * 2) - hex.length();
-
-        if(paddingLength > 0) {
-            return String.format("%0" + paddingLength + "d", 0) + hex;
-        } else {
-            return hex;
-        }
+    private static String toHex(byte[] bytes) {
+        return DatatypeConverter.printHexBinary(bytes);
     }
 
     private static boolean hashEquals(byte[] a, byte[] b) {
@@ -59,9 +48,9 @@ public class PasswordHash {
         random.nextBytes(salt);
 
         // Hash the password
-        byte[] hash = pbkdf2(password, salt, PBKDF2_ITERATIONS, HASH_BYTES);
+        byte[] hash = pbkdf2(password, salt, ITERATIONS, HASH_LEN);
         // Create pbkdf2 format
-        return PBKDF2_ITERATIONS + ":" + toHex(salt) + ":" +  toHex(hash);
+        return ITERATIONS + ":" + toHex(salt) + ":" +  toHex(hash);
     }
 
     public static boolean validatePassword(char[] password, String pbkdf2Expected) {
