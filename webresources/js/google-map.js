0c41142e4bb72d6bb873677ced10c532ff74542e
Nicholas Narsing <soren121@sorenstudios.com>
2017-03-30 16:47:12 -0400

    Enable basic functionality for search box

--- webresources/js/google-map.js
+++ webresources/js/google-map.js
@@ -1,7 +1,7 @@
 function initMap() {
     var mapEl = document.querySelector('#google-map');
     // Show all of US in default view
-    var map = new google.maps.Map(mapEl, {
+    window.map = new google.maps.Map(mapEl, {
         center: {lat: 39.05, lng: -94.34},
         zoom: 5
     });
@@ -10,15 +10,37 @@ function initMap() {
     if(navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
             // Set the map center to the user's location
-            map.setCenter({
+            window.map.setCenter({
                 lat: position.coords.latitude,
                 lng: position.coords.longitude
             });
 
             // Zoom in
-            map.setZoom(12);
+            window.map.setZoom(12);
         }, function() {
             alert('Geolocation failed :(');
         });
     }
 }
+
+document.getElementById('location-search').addEventListener('submit', function(e) {
+    // Disable normal form submission
+    e.preventDefault();
+
+    // Get search query from input box
+    var searchQuery = e.target.elements[0].value.trim();
+    if(searchQuery.length == 0) {
+        return true;
+    }
+
+    // Lookup location using the Google Maps Geocoder API
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({address: searchQuery}, function(results, status) {
+        if(status == 'OK') {
+            // Center the map on the found location
+            window.map.setCenter(results[0].geometry.location);
+            // Zoom in
+            window.map.setZoom(12);
+        }
+    });
+});
