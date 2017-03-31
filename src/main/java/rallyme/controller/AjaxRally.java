23221ffe398bb808cbb494b26df0ba4764e10311
Michael Melatti <melatti.michael@gmail.com>
2017-03-30 22:26:51 -0400

    Ajax Controller
    Added Gson dependency to Pom,
    Ajax Controller doPost -> print error
    Ajax Controller doGet -> get json string form Gson

--- src/main/java/rallyme/controller/AjaxRally.java
+++ src/main/java/rallyme/controller/AjaxRally.java
@@ -19,6 +19,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.google.gson.Gson;
+
 @WebServlet(name="AjaxRally", urlPatterns={"/AjaxRally"})
 public class AjaxRally extends TemplateServlet {
 
@@ -26,34 +28,25 @@ public class AjaxRally extends TemplateServlet {
      * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Map<String, Object> root = new HashMap<>();
 
-        //don't know where we want to redirect to???????????
-        try {
-            freemarker.getTemplate("registration.ftl").process(root, response.getWriter());
-        } catch(TemplateException ex) {
-            throw new RuntimeException(ex);
-        }
+        Rally[] ralliesArray;
+        String jsonString;
+		
+		try {
+			ralliesArray = Rally.getAllRallies();
+			Gson gson = new Gson();
+			jsonString = gson.toJson(ralliesArray); //convert Obj[] array to json string
+		} catch (UserException ex) {
+			ex.printStackTrace();
+		}
+
     }
 
     /**
      * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	
-        // Attempt to get all rallies in database
-		Rally[] ralliesArray;
-		try {
-			ralliesArray = Rally.getAllRallies();
-			// Store Rally[] array as object in session
-			request.getSession().setAttribute("rallies", ralliesArray);
-			
-			//don't know where we want to redirect to????
-			response.sendRedirect("/Dashboard");
-		} catch (UserException ex) {
-			ex.printStackTrace();
-		}
-		
+        response.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);		
     }
 
 }
