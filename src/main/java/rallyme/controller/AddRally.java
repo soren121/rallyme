29aa085de43d48a0cbc12553801e498019a2472f
vvvvg CHEN <vvvvg CHEN>
2017-03-31 11:58:03 -0400

    added addrally java file and addrally html temp file

---
+++ src/main/java/rallyme/controller/AddRally.java
@@ -0,0 +1,44 @@
+package rallyme.controller;
+
+import rallyme.core.TemplateServlet;
+import rallyme.model.User;
+import rallyme.exception.UserException;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.io.PrintWriter;
+
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet(name="AddRally", urlPatterns={"/AddRally"})
+public class AddRally extends TemplateServlet {
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Map<String, Object> root = new HashMap<>();
+
+        try {
+            freemarker.getTemplate("addrally.ftl").process(root, response.getWriter());
+        } catch(TemplateException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setStatus(HttpServletResponse.SC_METHOD_NOT_ALLOWED);
+    }
+
+}
