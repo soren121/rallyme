a0b8e2c2c8e82d69c333ff705fc534041bc8de2d
Nicholas Narsing <soren121@sorenstudios.com>
2017-04-14 01:19:50 -0400

    Restrict access to authenticated pages
    Redirect to the login page if the user isn't logged in

--- src/main/java/rallyme/core/Router.java
+++ src/main/java/rallyme/core/Router.java
@@ -2,14 +2,20 @@ package rallyme.core;
 
 import java.io.IOException;
 import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+@WebFilter(filterName="Router", urlPatterns={"/*"})
 public class Router implements Filter {
 
     @Override
@@ -18,9 +24,20 @@ public class Router implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain fc) throws ServletException, IOException {
         HttpServletRequest httpRequest = (HttpServletRequest)request;
-        String url = httpRequest.getRequestURL().toString();
+        HttpServletResponse httpResponse = (HttpServletResponse)response;
+        String uri = httpRequest.getRequestURI();
+        String servletPath = httpRequest.getServletPath();
 
-        // Add routing rules here
+        // Define regex for pages that require authentication
+        Pattern authRegex = Pattern.compile("^/(?:Dashboard|AddRally|EditRally|Profile)");
+        Matcher authMatcher = authRegex.matcher(servletPath);
+
+        // If this is an authenticated page but user isn't logged in, redirect to Login page
+        HttpSession session = httpRequest.getSession(false);
+        boolean noAuth = (session == null || session.isNew() || session.getAttribute("user") == null);
+        if(authMatcher.find() && noAuth) {
+            httpResponse.sendRedirect("Login");
+        }
 
         fc.doFilter(request, response);
     }
