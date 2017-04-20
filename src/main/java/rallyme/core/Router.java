fde4c1e2d3aa9635cae6a9f51e95198a9362d465
Nicholas Narsing <soren121@sorenstudios.com>
2017-04-20 19:10:19 -0400

    Route SPA paths to index.html

--- src/main/java/rallyme/core/Router.java
+++ src/main/java/rallyme/core/Router.java
@@ -31,12 +31,28 @@ public class Router implements Filter {
         // Define regex for pages that require authentication
         Pattern authRegex = Pattern.compile("^/(?:Dashboard|AddRally|EditRally|Profile)");
         Matcher authMatcher = authRegex.matcher(servletPath);
-
         // If this is an authenticated page but user isn't logged in, redirect to Login page
         HttpSession session = httpRequest.getSession(false);
         boolean noAuth = (session == null || session.isNew() || session.getAttribute("user") == null);
+
+        // Define regex for index page deeplinks
+        Pattern spaRegex = Pattern.compile("^.+/Rally/\\d+$");
+        Matcher spaMatcher = spaRegex.matcher(uri);
+
+        // Define regex for resources
+        Pattern resRegex = Pattern.compile("^.+/(?=js|images|css|AjaxRally$)");
+        Matcher resMatcher = resRegex.matcher(uri);
+
+        // Process URI filters
         if(authMatcher.find() && noAuth) {
             httpResponse.sendRedirect("Login");
+        } else if(resMatcher.find()) {
+            String newUri = "/" + resMatcher.replaceFirst("");
+            request.getRequestDispatcher(newUri).forward(request, response);
+            return;
+        } else if(spaMatcher.find()) {
+            request.getRequestDispatcher("/").forward(request, response);
+            return;
         }
 
         fc.doFilter(request, response);
