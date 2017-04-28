0e66c9e749572181ff3b589e2c9bd2a768ef2771
Nicholas Narsing <soren121@sorenstudios.com>
2017-04-27 20:38:09 -0400

    Add more rallies to SQL

--- database.sql
+++ database.sql
@@ -42,6 +42,10 @@ INSERT INTO `users` (`id`, `username`, `password`, `email`, `first_name`, `last_
 (1, 'admin', '$2a$10$7KuZRPvizaIrI5GsRkdtXOXRLi7evQ75j/oystWFyBta0eE8X6qtm', 'admin@example.com', 'RallyMe', 'Admin');
 
 # Insert default rallies
-INSERT INTO `rallies` (`id`, `creator_id`, `name`, `description`, `twitter_handle`, `start_time`, `location`, `latitude`, `longitude`) VALUES
-(1, 1, 'Tax March', 'Tell Trump to release his tax returns. What\'s he hiding?', 'taxmarch', '2017-04-15 16:00:00', 'The National Mall', 38.8892, -77.0523),
-(2, 1, 'March for Science', 'We are political. We value diversity. We won\'t put up with harassment.', 'ScienceMarchDC', '2017-04-22 16:00:00', 'The National Mall', 38.8892, -77.0523);
+INSERT INTO `rallies` (`id`, `creator_id`, `parent_id`, `name`, `type`, `description`, `twitter_handle`, `url`, `start_time`, `location`, `latitude`, `longitude`) VALUES
+(1,	1,	NULL,	'Tax March',	'national',	'Tell Trump to release his tax returns. What\'s he hiding?',	'taxmarch',	NULL,	'2017-04-29 16:00:00',	'Washington, DC 20037, USA',	38.889,	-77.052),
+(2,	1,	NULL,	'March for Science',	'national',	'We are political. We value diversity. We won\'t put up with harassment.',	'ScienceMarchDC',	NULL,	'2017-04-30 16:00:00',	'Washington, DC 20037, USA',	38.889,	-77.052),
+(3,	1,	2,	'March for Science, Athens',	'local',	'We are rallying in Athens that morning from 11 to 12 in front of the Federal Courthouse (and Post Office) downtown. This is a peaceful, nonpartisan, family friendly event. Please come out and add your voice to the chorus.',	'',	NULL,	'2017-04-30 16:00:00',	'Athens, GA 30601, USA',	33.9519,	-83.3576),
+(4,	1,	NULL,	'Women\'s March of Washington',	'local',	'We are the Women\'s March. For inquiries contact info@womensmarch.com.',	'womensmarch',	NULL,	'2017-05-10 16:00:00',	'Washington, DC 20230, USA',	38.8901,	-77.0364),
+(5,	1,	2,	'March for Science, Boston',	'local',	'#ScienceMarch #ScienceMarchBoston Donate: http://tinyurl.com/MfSBOS',	'Mrch4ScienceBOS',	NULL,	'2017-04-30 16:00:00',	'Boston, MA 02112, USA',	42.3601,	-71.0589),
+(6,	1,	2,	'March for Science, SF',	'local',	'Official March For Science SF Bay Area Collective. FAQ: http://marchforsciencesf.com/faq/ ',	'ScienceMarchSF',	NULL,	'2017-04-30 16:00:00',	'San Francisco, CA 94103, USA',	37.7749,	-122.419);
